b3b5ba97816b790a6c597b836e9d01f0128d210b
DzulfiqarSiraj <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-11-23 17:10:49 +0700

    Membuat middleware otorisasi menggunakan role

---
+++ src/middlewares/roleCheck.middleware.js
@@ -0,0 +1,13 @@
+const roleCheckMiddleWare = (role) => {
+  return (req, res, next) => {
+    if(req.user.role !== role){
+      return res.status(403).json({
+        success: false,
+        message: 'Forbidden Access'
+      })
+    }
+    next()
+  }
+}
+
+module.exports = roleCheckMiddleWare
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 
 const authMiddleware = require('../middlewares/auth.middleware')
+const roleCheckMiddleWare = require('../middlewares/roleCheck.middleware')
 
 
 router.use('/auth',require('./auth.router'))
-router.use('/admin/', authMiddleware, require('./admin'))
+router.use('/admin/', authMiddleware, roleCheckMiddleWare('Super Administrator'), require('./admin'))
+
 
 
 module.exports = router
