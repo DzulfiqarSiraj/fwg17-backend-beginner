63e4e75aacfc9430168a2946d62a4967a51ebf91
DzulfiqarSiraj <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-12-28 10:04:55 +0700

    add express.static

--- index.js
+++ index.js
@@ -11,9 +11,11 @@ const morgan = require('morgan')
 const app = express();
 
 app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 app.use(morgan('dev'));
 app.use(cors())
 app.use('/', require('./src/routers'))
+app.use('/uploads/products',express.static('uploads/products'))
 
 
 app.get('/', (req, res) => {
