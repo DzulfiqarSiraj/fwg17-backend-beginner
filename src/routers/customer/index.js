30189b7eadaebc732ceb68b4ce9287665a671a57
DzulfiqarSiraj <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-04-01 21:33:00 +0700

    feat: add order details router

--- src/routers/customer/index.js
+++ src/routers/customer/index.js
@@ -2,5 +2,6 @@ const customerRouter = require('express').Router()
 
 customerRouter.use('/profile',require('./profile.router'))
 customerRouter.use('/orders',require('./orders.router'))
+customerRouter.use('/order-details',require('./orderDetails.router'))
 
 module.exports = customerRouter
