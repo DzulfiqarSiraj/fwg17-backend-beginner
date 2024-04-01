ddcb63abe424bc3a12e2dd3f0c5fb657835138aa
DzulfiqarSiraj <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-04-01 21:30:59 +0700

    feat: update router

---
+++ src/routers/customer/orderDetails.router.js
@@ -0,0 +1,12 @@
+const orderDetailsRouter = require('express').Router()
+
+const orderDetailsController = require('../../controllers/admin/orderDetails.controller')
+
+orderDetailsRouter.get('/', orderDetailsController.getAllOrderDetails)
+orderDetailsRouter.get('/:id', orderDetailsController.getDetailOrderDetail)
+orderDetailsRouter.post('/', orderDetailsController.createOrderDetail)
+orderDetailsRouter.patch('/:id', orderDetailsController.updateOrderDetail)
+orderDetailsRouter.delete('/:id', orderDetailsController.deleteOrderDetail)
+
+
+module.exports = orderDetailsRouter
--- src/routers/customer/orders.router.js
+++ src/routers/customer/orders.router.js
@@ -2,7 +2,7 @@ const ordersRouter = require('express').Router()
 
 const ordersController = require('../../controllers/customer/orders.controller')
 
-// ordersRouter.get('/',ordersController.getAllOrders);
+ordersRouter.get('/',ordersController.getAllOrders);
 // ordersRouter.get('/:id', ordersController.getDetailOrder)
 ordersRouter.post('/',ordersController.createOrder);
 // ordersRouter.patch('/:id',ordersController.updateOrder);
