7ebf33d5bd375ecdfce9b43f8851cc81d0d23ff1
DzulfiqarSiraj <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-04-02 08:10:42 +0700

    feat: update config for cloudinary

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,40 +1,48 @@
-const multer = require('multer')
-const path = require('path')
+const multer = require('multer');
+const uuidv4 = require('uuid').v4;
+const path = require('path');
 
 const extension = {
-  'image/png' : '.png',
-  'image/jpg' : '.jpg',
-  'image/jpeg' : '.jpeg',
+	'image/png' : '.png',
+	'image/jpg' : '.jpg',
+	'image/jpeg' : '.jpeg',
 }
 
 const storage = (dest, filename) => multer.diskStorage({
-  destination: (req,file, cb) => {
-    cb(null, path.join('uploads',dest))
-  },
-  filename: (req,file,cb) => {
-    filename = new Date().getTime()
-    cb(null,`${filename}${extension[file.mimetype]}`)
-  }
+	filename: (req,file,cb) => {
+		const filename = uuidv4()
+		cb(null,`${filename}${extension[file.mimetype]}`)
+	}
 })
 
+// const storage = (dest, filename) => multer.diskStorage({
+// 	destination: (req,file, cb) => {
+// 		cb(null, path.join('uploads',dest))
+// 	},
+// 	filename: (req,file,cb) => {
+// 		const filename = uuidv4()
+// 		cb(null,`${filename}${extension[file.mimetype]}`)
+// 	}
+// })
+
 const fileFilter = (req, file, cb) => {
-  const allowedExt = Object.keys(extension)
-  if(!allowedExt.includes(file.mimetype)){
-    cb(new Error('Extension issue'), false)
-  }else{
-    cb(null, true)
-  }
+	const allowedExt = Object.keys(extension)
+	if(!allowedExt.includes(file.mimetype)){
+	cb(new Error('Extension issue'), false)
+	}else{
+	cb(null, true)
+	}
 }
 
 const maxSize = 1 * 1024 * 1024
 
 const uploadMiddleware = (type, file) => {
-  const processUpload = multer({
-    storage: storage(type,file),
-    fileFilter,
-    limits: {fileSize: maxSize}
-  })
-  return processUpload
+	const uploadProcess = multer({
+		storage: storage(type,file),
+		fileFilter,
+		limits: {fileSize: maxSize}
+	})
+	return uploadProcess
 }
 
 module.exports = uploadMiddleware
